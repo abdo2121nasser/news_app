282ab5419f2090d4a4f05909c2b3ff33bb8b56dc
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2024-09-02 16:50:02 +0300

    added function of delete favourite item

--- news_app/app/src/main/java/com/example/myapplication/adaptors/FavouriteAdaptor.kt
+++ news_app/app/src/main/java/com/example/myapplication/adaptors/FavouriteAdaptor.kt
@@ -2,18 +2,23 @@ package com.example.myapplication.adaptors
 
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.myapplication.FavouriteActivity
-import com.example.myapplication.MainActivity
 import com.example.myapplication.R
-import com.example.myapplication.databinding.ArticleListItemBinding
+import com.example.myapplication.databinding.ActivityFavourtiteBinding
 import com.example.myapplication.databinding.FavouriteListItemBinding
 import com.example.myapplication.news_model.FavouriteModel
-import com.example.myapplication.news_model.NewsModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class FavouriteAdaptor(val activity: FavouriteActivity, val model: FavouriteModel) :
     RecyclerView.Adapter<FavouriteAdaptor.ItemHolder>() {
@@ -45,6 +50,22 @@ class FavouriteAdaptor(val activity: FavouriteActivity, val model: FavouriteMode
             ShareCompat.IntentBuilder(activity).setType("text/plain")
                 .setChooserTitle("share link with:")
                 .setText(model.articles[position].url).startChooser()
+        }
+        holder.binding.favourite.setOnClickListener {
+            val docId: String = activity.getSharedPreferences(
+                "user_secrete_data",
+                AppCompatActivity.MODE_PRIVATE
+            ).getString("user_docId", "").toString()
+
+            val fireStore = Firebase.firestore
+            fireStore.collection("users").document(docId).collection("favourites")
+                .document(model.articles[position].docId).delete().addOnSuccessListener {
+                   Toast.makeText(activity, "deleted successfully", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    Toast.makeText(activity, "there was an error", Toast.LENGTH_SHORT).show()
+                    Log.d("favourite",it.toString())
+
+                }
         }
     }
 
