b227d21179f8dbdd9e5a5b93d6b50c4be93e72ea
Eng\Mohamed Omar <119747408+M0hamed-0mar@users.noreply.github.com>
2024-09-04 12:57:47 -0700

    Add files via upload

--- news_app/app/src/main/java/com/example/myapplication/call_interfaces/NewsCallable.kt
+++ news_app/app/src/main/java/com/example/myapplication/call_interfaces/NewsCallable.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication.call_interfaces
 
-import com.example.myapplication.models.NewsModel
+import com.example.myapplication.news_model.NewsModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface NewsCallable {
 
-    @GET("/v2/everything?q=bitcoin&apiKey=823bf7004dde449bbd82ba85dfd69843")
-    fun getArticles():Call<NewsModel>
+    @GET
+    fun getArticles(@Url url: String): Call<NewsModel>
+
 }
